c787c3c46bd327badb9ced53ec34a9a21760af78
Sksaddamhussaina0 <shaiksaddua0@gmail.com>
2018-01-25 13:06:54 +0530

    sqlserver

--- src/main/java/com/betabulls/book/ticket/app/TicketBookingManagementApplication.java
+++ src/main/java/com/betabulls/book/ticket/app/TicketBookingManagementApplication.java
@@ -15,25 +15,12 @@ import com.betabulls.book.ticket.app.Service.TicketBookingService;
 import com.betabulls.book.ticket.app.entity.Ticket;
 
 @SpringBootApplication
-public class TicketBookingManagementApplication implements CommandLineRunner{
-	@Autowired()
-	public TicketBookingService ticketService;
-	@Autowired()
-	private DataSource dataSource;
+public class TicketBookingManagementApplication {
+	
+	
 	public static void main(String[] args) {
 	SpringApplication.run(TicketBookingManagementApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		Ticket ticket1=new Ticket();
-		ticket1.setPassengerName("devagoud");
-		ticket1.setBookingDate(new Date());
-		ticket1.setStationSource("kamareddy");
-		ticket1.setDestination("hyderabad");
-		ticket1.setEmail("RR@gmail.com");
-		ticketService.createTicket(ticket1);
-	System.out.println("DataSource:  "+dataSource);
-	}
+
 }
--- src/main/java/com/betabulls/book/ticket/app/entity/Ticket.java
+++ src/main/java/com/betabulls/book/ticket/app/entity/Ticket.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Ticket")
+@Table(name="ticket")
 
 public class Ticket {
 	
